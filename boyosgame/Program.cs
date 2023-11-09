679c2886e40cba4e0738940b2ad955f5bb4ce9b2
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-11-09 10:43:48 +0100

    vinterprojekt

---
+++ boyosgame/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Raylib_cs;
+Console.WriteLine("Hello, World!");
+
+Raylib.InitWindow(800, 600, "game");
+Raylib.SetTargetFPS(60);
