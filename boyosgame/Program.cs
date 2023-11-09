7360aa4a564715db7a29dc9a767b72f119595276
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-11-09 13:17:08 +0100

    RECTANGLE

--- boyosgame/Program.cs
+++ boyosgame/Program.cs
@@ -3,15 +3,23 @@
 
 Raylib.InitWindow(800, 600, "game");
 Raylib.SetTargetFPS(60);
+Rectangle hdjhd = new Rectangle(10, 10, 200, 200);
 
 while (!Raylib.WindowShouldClose())
 {
-  Raylib.BeginDrawing();
+    Raylib.BeginDrawing();
 
-  Raylib.ClearBackground(new Color(152,0,1,100));
+    int characterPositionx = 400;
+    int characterPositiony = 300;
 
-  Raylib.DrawRectangle( 500, 200, 32, 32, Color.BLACK);
+    Raylib.ClearBackground(new Color(152, 0, 1, 100));
+
+    //   Raylib.DrawRectangle( characterPositionx, characterPositiony, 32, 32, Color.BLACK);
+    Raylib.DrawRectangleRec(hdjhd, Color.GREEN);
 
 
-  Raylib.EndDrawing();
+
+
+
+    Raylib.EndDrawing();
 }
