12406b8fefae166b1f85ab8299cd6ab9a98fefc1
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-11-09 14:43:37 +0100

    big rectangle

--- boyosgame/Program.cs
+++ boyosgame/Program.cs
@@ -1,9 +1,16 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
 
 Raylib.InitWindow(800, 600, "game");
 Raylib.SetTargetFPS(60);
-Rectangle hdjhd = new Rectangle(10, 10, 200, 200);
+
+Vector2 movement = new Vector2(0.1f, 0.1f);
+
+float speed = 5;
+
+
+Rectangle playerRect = new Rectangle(400, 300, 32, 32);
 
 while (!Raylib.WindowShouldClose())
 {
@@ -12,10 +19,37 @@ while (!Raylib.WindowShouldClose())
     int characterPositionx = 400;
     int characterPositiony = 300;
 
-    Raylib.ClearBackground(new Color(152, 0, 1, 100));
+    Raylib.ClearBackground(Color.GRAY);
 
-    //   Raylib.DrawRectangle( characterPositionx, characterPositiony, 32, 32, Color.BLACK);
-    Raylib.DrawRectangleRec(hdjhd, Color.GREEN);
+    Raylib.DrawRectangleRec(playerRect, Color.RED);
+
+    movement = Vector2.Zero;
+
+     if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+    {
+      movement.X = -1;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+    {
+      movement.X = 1;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+    {
+      movement.Y = -1;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+    {
+      movement.Y = 1;
+    }
+
+    if (movement.Length() > 0)
+    {
+      movement = Vector2.Normalize(movement) * speed;
+    }
+
+    playerRect.X += movement.X;
+    playerRect.Y += movement.Y;
+
 
 
 
